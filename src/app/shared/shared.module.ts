68e29de86db52e078a4cec00f52a36566d61fb09
Romero Alejo <alerom2206@gmail.com>
2025-10-01 15:40:33 -0300

    modifica: carpeta shared, agrega: estilos generales en styles.css

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { SharedComponent } from './shared.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PopUpComponent } from './components/pop-up/pop-up.component';
+
+
+@NgModule({
+  declarations: [
+    SharedComponent,
+    FooterComponent,
+    HeaderComponent,
+    PopUpComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
